621cba6f6d11ce0f493d57de25d1868211bbde93
ChristianDerouen <ccd1549989@lpssonline.com>
2022-10-07 18:37:34 +0000

    <message about commit>

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,12 +37,11 @@
     createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
 
     // example:
-    createPlatform(600, 250,0.5);
-    createPlatform(400, 300, 0.5);
-    createPlatform(30, 400, 0.6);
-    createPlatform(150, 600, 0.5);
-    createPlatform(600, 600,0.5);
-
+    createPlatform(200, 270, 0.5)
+    createPlatform(350, 350, 0.6)
+    createPlatform(400, 450, 0.7)
+    createPlatform(650, 490, 0.4)
+    createPlatform(400, 580, 0.4)
   
 
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
